527ffc8d986205dce18a4c4815cb6bf37d0efd7b
sashamoon <sarah.alves8@hotmail.com>
2022-01-26 16:59:13 -0300

    :package: Inserção de animação na página de produtos e contato.

--- src/js/produtos.main.js
+++ src/js/produtos.main.js
@@ -12,3 +12,41 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
 $('.hamburguer').click(function(){
     $('body').hasClass('show-menu') ? $('#teste').removeClass('bg-header') : $('#teste').addClass('bg-header');
 });
+
+//Animação quando scrolla 
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function() {
+            timeout = null;
+            if(!immediate) func.apply(context, args)
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow) func.apply(context, args);
+    };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+        animeScroll();
+    }, 200));
+}
